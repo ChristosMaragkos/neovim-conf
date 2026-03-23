83fe32d4e36906ee97a5b4dd74312057f3fa94e9
Christos Maragkos <whitetowersoftware@gmail.com>
2026-03-23 14:13:37 +0200

    use better language server for C#

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,17 +1,43 @@
 return {
-  -- 1. Configure LSP to swap servers
+  -- Fix Mason by including BOTH the default and the Roslyn registry
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",    -- The one you were missing!
+        "github:Crashdummyy/mason-registry", -- The one for Roslyn
+      },
+    },
+  },
+
+  -- Configure the Roslyn plugin
+  {
+    "seblyng/roslyn.nvim",
+    ft = "cs",
+    opts = {
+      exe = {
+        "dotnet",
+        vim.fn.stdpath("data") .. "/mason/packages/roslyn/Microsoft.CodeAnalysis.LanguageServer.dll",
+      },
+      config = {
+        -- This helps Roslyn find your Godot .sln automatically
+        settings = {
+          ["csharp|background_analysis"] = {
+            dotnet_compiler_diagnostics_scope = "fullSolution",
+            dotnet_analyzer_diagnostics_scope = "fullSolution",
+          },
+        },
+      },
+    },
+  },
+
+  -- Disable the old servers in lspconfig
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        -- Disable OmniSharp (The one crashing)
         omnisharp = { enabled = false },
-        
-        -- Enable csharp_ls (The stable one)
-        csharp_ls = {
-          -- Ensure it handles standard C# files
-          filetypes = { "cs" },
-        },
+        csharp_ls = { enabled = false },
       },
     },
   },
