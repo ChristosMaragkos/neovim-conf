50627dcd979c5b29a9c602207d0bfa80e3d6c954
Christos Maragkos <whitetowersoftware@gmail.com>
2026-04-01 03:21:31 +0300

    Disable razor explicitly to reduce Inotify capacity bloat

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -30,6 +30,41 @@ return {
 			},
 		},
 	},
+	-- Fix Mason by including BOTH the default and the Roslyn registry
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			registries = {
+				"github:mason-org/mason-registry",
+				"github:Crashdummyy/mason-registry", -- The one for Roslyn
+			},
+		},
+	},
+
+	-- Configure the Roslyn plugin
+	{
+		"seblyng/roslyn.nvim",
+		ft = "cs",
+		opts = {
+			exe = {
+				"dotnet",
+				vim.fn.stdpath("data") .. "/mason/packages/roslyn/Microsoft.CodeAnalysis.LanguageServer.dll",
+			},
+			config = {
+				settings = {
+					["csharp|background_analysis"] = {
+						dotnet_compiler_diagnostics_scope = "fullSolution",
+						dotnet_analyzer_diagnostics_scope = "fullSolution",
+					},
+					razor = {
+						language_server = {
+							cohosting_enabled = false,
+						},
+					},
+				},
+			},
+		},
+	},
 
 	-- Disable the old servers in lspconfig
 	{
