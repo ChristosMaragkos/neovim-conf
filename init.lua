958cf3ea9f08bec737f3df084ff244cf386423ff
Christos Maragkos <whitetowersoftware@gmail.com>
2026-06-10 22:14:22 +0300

    Switch colorscheme (again)
    This time I'm using a darkened version of everforest. Looks good and
    doesn't hurt my eyes. I might even stick with it.

--- init.lua
+++ init.lua
@@ -13,3 +13,15 @@ vim.filetype.add({
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
+
+vim.g.everforest_background = "hard"
+vim.g.everforest_colors_override = {
+	bg_dim = { "#14191C", "NONE" },
+	bg0 = { "#1B2227", "NONE" },
+	bg1 = { "#222C30", "NONE" },
+	bg2 = { "#2B3539", "NONE" },
+	bg3 = { "#333D42", "NONE" },
+	bg4 = { "#3B4348", "NONE" },
+	bg5 = { "#414D4A", "NONE" },
+}
+vim.cmd("colorscheme everforest")
