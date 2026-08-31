f9039e63225f608bd9de066fb779812c92a96eea
Christos Maragkos <whitetowersoftware@gmail.com>
2026-08-31 17:24:51 +0300

    Remove redundant markdown lsp silencer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,13 +6,3 @@ vim.filetype.add({
 		asm = "masm",
 	},
 })
--- stop the freaking markdown LSP from badgering us with warnings.
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.diagnostic.enable(false)
-		vim.opt.conceallevel = 2
-		vim.opt_local.wrap = false
-		vim.opt_local.sidescrolloff = 8
-	end,
-})
